256fd302be77b31ba41e6ff1f4764e0244d5dfa2
dande <7bdande@gmail.com>
2020-12-17 22:51:01 +0900

    feat: 상수 추가

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const ID = {
+  CAR_NAMES_CONTAINER: "car-names-container",
+  CAR_NAMES_INPUT: "car-names-input",
+  CAR_NAMES_BUTTON: "car-names-submit",
+  RACING_COUNT_CONTAINER: "racing-count-container",
+  RACING_COUNT_INPUT: "racing-count-input",
+  RACING_COUNT_BUTTON: "racing-count-button",
+  GAME_RESULT_CONTAINER: "game-result-container",
+};
+
+export const ALERT = {
+  NAME_INPUT_ALERT: "5자 이하의 이름을 콤마로 구분하여 입력해주세요.",
+  DUPLICATED_NAME_ALERT: "중복된 이름이 존재합니다.",
+  INCORRECT_NUMBER_ALERT:
+    "최소 한 번 이상 시도해야 합니다. 1 이상의 수를 입력해주세요.",
+};
+
+export const NUMBER = {
+  MAX_CAR_NAME_LENGTH: 5,
+  MIN_RANDOM_NUMBER: 0,
+  MAX_RANDOM_NUMBER: 9,
+  ADD_STEP_STANDARD: 4,
+};
