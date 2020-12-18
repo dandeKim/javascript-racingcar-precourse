e2a8d6e07630837164d8b4575c490a1bef4b3ce7
dande <7bdande@gmail.com>
2020-12-18 10:33:13 +0900

    feat: [CarNameForm] 확인 버튼에 클릭 이벤트 등록

--- src/modules/CarNameForm.js
+++ src/modules/CarNameForm.js
@@ -1,3 +1,27 @@
+import { ID } from "../utils/constants.js";
+import { getCarNamesList } from "../utils/carNameUtil.js";
+import { isValidCarNames } from "../utils/validations.js";
+
 export default class CarNameForm {
-  constructor() {}
+  constructor(setPlayers) {
+    this.setPlayers = setPlayers;
+
+    this.render();
+  }
+
+  handleCarNamesButton = () => {
+    const $carNamesInput = document.querySelector(`#${ID.CAR_NAMES_INPUT}`);
+    const carNamesList = getCarNamesList($carNamesInput.value);
+
+    if (!isValidCarNames($carNamesInput, carNamesList)) {
+      return;
+    }
+
+    this.setPlayers(carNamesList);
+  };
+
+  render = () => {
+    const $carNamesButton = document.querySelector(`#${ID.CAR_NAMES_BUTTON}`);
+    $carNamesButton.addEventListener("click", this.handleCarNamesButton);
+  };
 }
