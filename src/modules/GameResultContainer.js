a112957eaf28b05d3d4d9e082184679a6a483616
dande <7bdande@gmail.com>
2020-12-18 11:43:47 +0900

    feat: [GameResultContainer] 게임 결과 HTML 생성

--- src/modules/GameResultContainer.js
+++ src/modules/GameResultContainer.js
@@ -1,3 +1,20 @@
+import { ID } from "../utils/constants.js";
+import { playerHTML } from "../utils/templates.js";
+
 export default class GameResultContainer {
-  constructor() {}
+  constructor(getPlayers, getWinners) {
+    this.getPlayers = getPlayers;
+    this.getWinners = getWinners;
+  }
+
+  createResultHTML = () => {
+    const players = this.getPlayers();
+    const result = players
+      .map(player => {
+        return playerHTML(player.getName(), player.getStep());
+      })
+      .join("");
+
+    return `<div>${result}</div>`;
+  };
 }
